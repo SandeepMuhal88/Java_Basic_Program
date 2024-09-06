772e69cbbcb2411ff5f69c50744088061347b1bf
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-06 09:13:47 +0530

    python basic code

---
+++ basic.java
@@ -0,0 +1,10 @@
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("Hellow world in this aera:))");
+        // Vriable ...
+        float f=12.50f;
+        int a=45;
+        char ch='o';
+        System.out.println("Integer value:- "+a+" "+"Floating value= "+f+" "+"Character value= "+ch+" ");
+    }
+}
